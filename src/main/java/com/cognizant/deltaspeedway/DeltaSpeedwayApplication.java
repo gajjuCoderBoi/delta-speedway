83160f555e95d1fc98cdf06065ac7017ea35a0ca
Mohammad Javed <ghazanfar9131@gmail.com>
2021-04-19 19:16:06 -0400

    init gradle

---
+++ src/main/java/com/cognizant/deltaspeedway/DeltaSpeedwayApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.deltaspeedway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaSpeedwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeltaSpeedwayApplication.class, args);
+    }
+
+}
